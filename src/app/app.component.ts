c38992d9f407318b6dec90e481472cdc11beeb3e
Albertomarquezzz <103041156+Albertomarquezzz@users.noreply.github.com>
2023-01-24 13:05:55 +0100

    v1

---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TAREA1';
+  public color:string;
+  public var1:number;
+  public aciertos:number;
+  public fallos:number;
+  public id:number;
+  constructor(){
+    this.color = '';
+    this.var1 = 0;
+    this.aciertos = 0;
+    this.fallos = 0;
+    this.id = 0;
+  }
+
+  cambiarcolor(){
+    var random = Math.floor(Math.random() * 4) + 1;
+    this.var1 = random;
+    if(random == 1){
+      this.color = 'red'
+    }
+    else if(random == 2){
+      this.color = 'yellow'
+    }
+    else if(random == 3){
+      this.color = 'green'
+    }
+    else if(random == 4){
+      this.color = 'blue'
+    } 
+  }
+
+  comprobar(x:number){
+    if(this.color == 'blue' && x == 2){
+      this.aciertos++
+    }
+    else if(this.color == 'blue' && x != 2){
+      this.fallos++
+    }
+    else if(this.color == 'red' && x == 0){
+      this.aciertos++
+    }
+    else if(this.color == 'red' && x != 0){
+      this.fallos++
+    }
+    else if(this.color == 'green' && x == 3){
+      this.aciertos++
+    }
+    else if(this.color == 'green' && x != 3){
+      this.fallos++
+    }
+    else if(this.color == 'yellow' && x == 1){
+      this.aciertos++
+    }
+    else if(this.color == 'yellow' && x != 1){
+      this.fallos++
+    }
+    this.cambiarcolor()
+  }
+}
